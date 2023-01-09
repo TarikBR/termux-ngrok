a7a758612546d4aa2ec7831f514593018ba505be
TarikBR <64391349+TarikBR@users.noreply.github.com>
2023-01-09 10:44:06 -0300

    Upload de scripts

---
+++ install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+rm $PREFIX/bin/ngrok
+rm -rf $PREFIX/share/ngrok
+mkdir -p $PREFIX/share/ngrok
+cp get-ngrok.sh $PREFIX/share/ngrok
+cp ngrok $PREFIX/bin
+apt update && apt upgrade -y
+apt install -y proot wget resolv-conf
+apt clean
+apt autoremove
+cd $PREFIX/share/ngrok
+
+echo "Baixando ngrok..."
+case `dpkg --print-architecture` in
+aarch64)
+    architectureURL="arm64" ;;
+arm)
+    architectureURL="arm" ;;
+armhf)
+    architectureURL="arm" ;;
+amd64)
+    architectureURL="amd64" ;;
+i*86)
+    architectureURL="386" ;;
+x86_64)
+    architectureURL="amd64" ;;
+*)
+    echo "Dispositivo não suportado"
+esac
+
+wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-stable-linux-${architectureURL}.zip -O ngrok.zip
+unzip ngrok.zip
+rm ngrok.zip
+
+
+echo -e "\e[1;32mNgrok instalado com sucesso!"
+echo "Rode : ngrok "
+echo " Para usar o ngrok"
